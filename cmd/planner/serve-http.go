3c3c42a4b9f4f968ac63a86cc6df2dfbe73c9974
Maartje Eyskens <maartje@eyskens.me>
2022-08-13 20:49:44 +0200

    Deploy the HTTP server

--- cmd/planner/serve-http.go
+++ cmd/planner/serve-http.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"github.com/meyskens/m-planner/pkg/db"
 	"github.com/spf13/cobra"
 )
 
@@ -19,6 +20,15 @@ func init() {
 type serveHTTPCmdOptions struct {
 	BindAddr string
 	Port     int
+
+	db *db.Connection
+
+	postgresHost     string
+	postgresPort     int
+	postgresUsername string
+	postgresDatabase string
+	postgresPassword string
+	enableSSL        bool
 }
 
 // NewServeHTTPCmd generates the `serve-http` command
@@ -35,6 +45,18 @@ func NewServeHTTPCmd() *cobra.Command {
 	c.Flags().StringVarP(&s.BindAddr, "bind-address", "b", "0.0.0.0", "address to bind port to")
 	c.Flags().IntVarP(&s.Port, "port", "p", 8080, "Port to listen on")
 
+	c.Flags().StringVar(&s.postgresHost, "postgres-host", "", "PostgreSQL hostname")
+	c.Flags().IntVar(&s.postgresPort, "postgres-port", 5432, "PostgreSQL hostname")
+	c.Flags().StringVar(&s.postgresUsername, "postgres-username", "", "PostgreSQL hostname")
+	c.Flags().StringVar(&s.postgresPassword, "postgres-password", "", "PostgreSQL hostname")
+	c.Flags().StringVar(&s.postgresDatabase, "postgres-database", "", "PostgreSQL hostname")
+	c.Flags().BoolVar(&s.enableSSL, "postgres-enable-ssl", false, "PostgreSQL to use TLS")
+
+	c.MarkFlagRequired("postgres-host")
+	c.MarkFlagRequired("postgres-username")
+	c.MarkFlagRequired("postgres-password")
+	c.MarkFlagRequired("postgres-database")
+
 	return c
 }
 
@@ -44,6 +66,19 @@ func (s *serveHTTPCmdOptions) Validate(cmd *cobra.Command, args []string) error 
 
 func (s *serveHTTPCmdOptions) RunE(cmd *cobra.Command, args []string) error {
 	ctx, cancel := context.WithCancel(context.Background())
+
+	var err error
+	s.db, err = db.NewConnection(db.ConnectionDetails{
+		Host:      s.postgresHost,
+		Port:      s.postgresPort,
+		User:      s.postgresUsername,
+		Password:  s.postgresPassword,
+		Database:  s.postgresDatabase,
+		EnableSSL: s.enableSSL,
+	})
+	if err != nil {
+		return fmt.Errorf("error connecting to DB %w", err)
+	}
 
 	e := echo.New()
 	e.HideBanner = true
